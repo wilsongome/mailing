42398e5f3e0aef45aece3a9587c097489e7df0e3
Wilson <wilsongome@gmail.com>
2023-06-21 22:36:03 -0300

    Layout configurado ADM LTE

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+@extends('adminlte::page')
+
+@section('title', 'Sistema de Mailing')
+
+@section('content_header')
+    <h1>Mailing</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
