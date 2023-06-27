fe89198203a466c2d268f5e72d622e57da6107ea
Wilson <wilsongome@gmail.com>
2023-06-27 18:58:26 -0300

    Migrate update email template (title, body)

---
+++ database/migrations/2023_06_27_214952_add_title_and_body_to_email_template_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('email_templates', function (Blueprint $table) {
+            $table->char('title', 100)->after('description')->nullable();
+            $table->longText('body')->after('title')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('email_templates', function (Blueprint $table) {
+            $table->dropColumn('title');
+            $table->dropColumn('body');
+        });
+    }
+};
