d8850ef0fa6f892f9b32bb640a923c01b8b6fe5d
Wilson Gomes <wilsongome@gmail.com>
2023-11-03 16:05:20 -0300

    Todas ass Rotas autenticadas

--- routes/web.php
+++ routes/web.php
@@ -56,13 +56,15 @@ Route::get('/login',[AuthController::class, 'login'])->name("login");
 Route::post('/login',[AuthController::class, 'authenticate'])->name("login");
 Route::post('/logout',[AuthController::class, 'logout'])->name("logout");
 
-Route::controller(UserController::class)->group(function (){
-    Route::get('/user', 'index')->name('user.index');
-    Route::get('/user/create','create')->name("user.create");
-    Route::get('/user/{id}/edit','edit')->name("user.edit");
-    Route::post('/user','store')->name("user.store");
-    Route::match(['put', 'patch'],'/user/{id}', 'update')->name('user.update');
-    Route::delete('/user/{id}','destroy')->name('user.destroy');
+Route::middleware(Authenticator::class)->group(function (){
+    Route::controller(UserController::class)->group(function (){
+        Route::get('/user', 'index')->name('user.index');
+        Route::get('/user/create','create')->name("user.create");
+        Route::get('/user/{id}/edit','edit')->name("user.edit");
+        Route::post('/user','store')->name("user.store");
+        Route::match(['put', 'patch'],'/user/{id}', 'update')->name('user.update');
+        Route::delete('/user/{id}','destroy')->name('user.destroy');
+    });
 });
 
 Route::middleware(Authenticator::class)->group(function (){
@@ -79,26 +81,31 @@ Route::middleware(Authenticator::class)->group(function (){
 });
 
 
-
-Route::controller(CampaignHistoryController::class)->group(function (){
-    Route::get('/campaign/{id}/history', 'index')->name('campaign.history');
+Route::middleware(Authenticator::class)->group(function (){
+    Route::controller(CampaignHistoryController::class)->group(function (){
+        Route::get('/campaign/{id}/history', 'index')->name('campaign.history');
+    });
 });
 
-Route::controller(EmailTemplateController::class)->group(function (){
-    Route::get('/email_template', 'index')->name('email_template.index');
-    Route::get('/email_template/create', 'create')->name('email_template.create');
-    Route::get('/email_template/{id}/edit', 'edit')->name('email_template.edit');
-    Route::post('/email_template', 'store')->name('email_template.store');
-    Route::match(['put', 'patch'],'/email_template/{id}', 'update')->name('email_template.update');
-    Route::delete('/email_template/{id}', 'destroy')->name('email_template.destroy');
+Route::middleware(Authenticator::class)->group(function (){
+    Route::controller(EmailTemplateController::class)->group(function (){
+        Route::get('/email_template', 'index')->name('email_template.index');
+        Route::get('/email_template/create', 'create')->name('email_template.create');
+        Route::get('/email_template/{id}/edit', 'edit')->name('email_template.edit');
+        Route::post('/email_template', 'store')->name('email_template.store');
+        Route::match(['put', 'patch'],'/email_template/{id}', 'update')->name('email_template.update');
+        Route::delete('/email_template/{id}', 'destroy')->name('email_template.destroy');
+    });
 });
 
-Route::controller(ContactListController::class)->group(function (){
-    Route::get('/contact_list', 'index')->name('contact_list.index');
-    Route::get('/contact_list/create', 'create')->name('contact_list.create');
-    Route::get('/contact_list/{id}/edit', 'edit')->name('contact_list.edit');
-    Route::post('/contact_list', 'store')->name('contact_list.store');
-    Route::match(['put', 'patch'],'/contact_list/{id}', 'update')->name('contact_list.update');
-    Route::delete('/contact_list/{id}', 'destroy')->name('contact_list.destroy');
-    Route::get('/contact_list/{id}/download', 'download')->name('contact_list.download');
+Route::middleware(Authenticator::class)->group(function (){
+    Route::controller(ContactListController::class)->group(function (){
+        Route::get('/contact_list', 'index')->name('contact_list.index');
+        Route::get('/contact_list/create', 'create')->name('contact_list.create');
+        Route::get('/contact_list/{id}/edit', 'edit')->name('contact_list.edit');
+        Route::post('/contact_list', 'store')->name('contact_list.store');
+        Route::match(['put', 'patch'],'/contact_list/{id}', 'update')->name('contact_list.update');
+        Route::delete('/contact_list/{id}', 'destroy')->name('contact_list.destroy');
+        Route::get('/contact_list/{id}/download', 'download')->name('contact_list.download');
+    });
 });
