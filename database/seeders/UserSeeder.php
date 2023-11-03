d846ae6ed4d3c945557c6e9c20badf0ee0dcfa94
Wilson Gomes <wilsongome@gmail.com>
2023-11-03 15:44:30 -0300

    Criando usuário DEFAULT (seed)

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => "Admin",
+            'email' => "admin@mailing.com",
+            'password' => Hash::make('admin'),
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
+    }
+}
